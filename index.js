affc9750087a884a7ec0d03ecfbbc9bc2a5c27bb
Gabriel <44093861+GaboAlex@users.noreply.github.com>
2019-01-28 11:34:10 -0500

    testing
    testing

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 const Sequelize = require('sequelize');
 var models = require("./models");
 var app = require('./app');
-var puerto = 3700;
+// var puerto = 3700;
+var port = app.set('port', process.env.PORT || 3000);
 
 //Sync Database
 models.sequelize.sync().then(()=>{
     console.log("Conexion con la base de datos DB_Mikhuna exitosa!!");
-    app.listen(puerto,()=>{
+    app.listen(port,()=>{
         console.log("Servidor corriendo perfectamente => localhost:3700");
     });
 }).catch(err=>{
