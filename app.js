201129af761ad4f58fa60957f18a0d9e6d812a1d
diegozeta <dzegarram@unsa.edu.pe>
2019-02-05 11:09:57 -0500

    make it better

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var app = express();
 
 // require('./api/config/passport');
 // app.use(passport.initialize());
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('funcionando');
 });
 
 
-//RUTAS
+// RUTAS
 var rutas_de_categoria = require('./routes/categoriaRoutes');
 var rutas_de_plato = require('./routes/platoRoutes');
 var rutas_de_usuario = require('./routes/usuarioRoutes');
@@ -24,28 +24,32 @@ var rutas_de_pedido = require('./routes/pedidoRoutes');
 var rutas_de_menuPlato = require('./routes/menuPlatoRoutes');
 var rutas_de_pedidoDetalle = require('./routes/pedidoDetalleRoutes');
 
-//MIDDLEWARES
-app.use(bodyparser.urlencoded({extended:false}));
+// MIDDLEWARES
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
 app.use(bodyparser.json());
 app.use((req, res, next) => {
-    //para permitir quien podrá conectarse al servidor
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  // para permitir quien podrá conectarse al servidor
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
 
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401);
-      res.json({"message" : err.name + ": " + err.message});
-    }
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({
+      "message": err.name + ": " + err.message
+    });
+  }
 });
 
 // cargando archivo de rutas
 
-app.get('/',function(req, res, next){
+app.get('/', function(req, res, next) {
   res.send('hola ')
 });
 app.use('/API', rutas_de_categoria);
